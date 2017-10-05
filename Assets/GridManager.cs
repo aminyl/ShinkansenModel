0cae46e323a184c7d62fd9d90054f514634e1cc7
aminyl <ellallopll@gmail.com>
2017-10-06 00:41:13 +0900

    no message

---
+++ Assets/GridManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour {
+	public SeatGenerator seatGenerator;
+	int[,] grid;
+
+	// Use this for initialization
+	void Start () {
+		grid = new int[seatGenerator.ROW_N, seatGenerator.COL_LEFT_N + seatGenerator.COL_RIGHT_N];
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	Vector3 GridToField(int i, int j){
+		float x, z;
+		if (i < seatGenerator.COL_LEFT_N)
+			x = i * seatGenerator.WIDTH;
+		else
+			x = seatGenerator.CENTER_MERGIN + seatGenerator.WIDTH * seatGenerator.COL_LEFT_N + (i - seatGenerator.COL_LEFT_N) * seatGenerator.WIDTH;
+		z = j * seatGenerator.PITCH;
+		return new Vector3(x, 0, -z);
+	}
+}
---
+++ Assets/SeatGenerator.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeatGenerator : MonoBehaviour {
+	public float PITCH = 1.04f;
+	public float WIDTH = 0.44f;
+	public float CENTER_MERGIN = 0.57f;
+	public int ROW_N = 20;
+	public int COL_LEFT_N = 3;
+	public int COL_RIGHT_N = 2;
+
+	public GameObject seat;
+
+	// Use this for initialization
+	void Start () {
+		GenerateSeats();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void GenerateSeats(){
+		for(int i = 0; i < COL_LEFT_N; i++){
+			for(int j = 0; j < ROW_N; j++){
+				GameObject _seat = Instantiate(seat);
+				_seat.transform.position = new Vector3(i * WIDTH, 0, - j * PITCH);
+			}
+		}
+		float _offset = CENTER_MERGIN + WIDTH * COL_LEFT_N;
+		for(int i = 0; i < COL_RIGHT_N; i++){
+			for(int j = 0; j < ROW_N; j++){
+				GameObject _seat = Instantiate(seat) as GameObject;
+					_seat.transform.position = new Vector3(i * WIDTH + _offset, 0, - j * PITCH);
+			}
+		}
+	}
+}
